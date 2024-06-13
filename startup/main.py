4754287f8dcad5c0372645804cec9aabca138633
ajitcool19 <ajitbhalerao74@gmail.com>
2024-06-13 18:24:04 +0530

    moved middleware list to config

--- startup/main.py
+++ startup/main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from common.middleware.middleware_manager import Middleware
 from common.routes import Routes
 from common.monitoring.sentry import init_sentry
-
+from common.config.config import current_config
 init_sentry() # Comment this line if you don't want to use sentry
 
 
 app = FastAPI()
-Middleware(app, ["login","db_session","timer","auth"])
+Middleware(app, current_config.MIDDLEWARES)
 
 
 Routes(app).include_routes()
